9b19374a4ba755e2cdbd0bd0b043c958db7c74dd
MarcusEcko <marco237.mnm@gmail.com>
2026-03-05 14:07:35 -0300

    task priority added

--- src/Components/TaskForm.jsx
+++ src/Components/TaskForm.jsx
@@ -5,6 +5,7 @@ import { Button, Form } from "react-bootstrap";
 export default function TaskForm ({ addTasks }) {
     
     const [text, setText] = useState("");
+    const [priority, setPriority] = useState("");
 
     const HandleSubmit = (e) => {
         e.preventDefault();
@@ -13,6 +14,7 @@ export default function TaskForm ({ addTasks }) {
             id: crypto.randomUUID(),
             text: text,
             completed: false,
+            priority: priority,
             createdAt: Date.now()
         }
 
@@ -29,6 +31,12 @@ export default function TaskForm ({ addTasks }) {
                     value={text}
                     onChange={e => setText(e.target.value)}
                 />
+                <Form.Select className="mt-2 mb-2">
+                    <option>Select priority</option>
+                    <option value={1} onClick={() => setPriority("low")}>Low</option>
+                    <option value={2} onClick={() => setPriority("medium")}>Medium</option>
+                    <option value={3} onClick={() => setPriority("high")}>High</option>
+                </Form.Select>
             </Form.Group>
 
             <Button 
--- src/Components/TaskItem.jsx
+++ src/Components/TaskItem.jsx
@@ -1,18 +1,34 @@
-import { Button, Card } from "react-bootstrap";
+import { Badge, Button, Card } from "react-bootstrap";
 
 
-export default function TaskItem ({ task, onComplete, onDelete, onEdit}) {
+export default function TaskItem ({ task, onComplete, onDelete, onEdit }) {
+    const priorityColor = () => {
+        if(task.priority === "low"){
+            return "success";
+        } else if( task.priority === "medium"){
+            return "warning"
+        } else{
+            return "danger"
+        }
+    }
+    
     return(
         <Card>
             <Card.Body>
-                <Card.Title>{task.text}</Card.Title>
+                <Card.Title>
+                    <Badge className="me-2" bg={priorityColor()}>
+                        {task.priority}
+                    </Badge>
+                    {task.text}
+                </Card.Title>
                 <Card.Text>
                     Date: {new Date(task.createdAt).toLocaleDateString()}<br/>
                     Completed: 
                     <input 
                         type="checkbox"
                         checked={task.completed}
-                        onChange={() => onComplete(task.id)}/>
+                        onChange={() => onComplete(task.id)}
+                    />
                 </Card.Text>
                 <div className="d-flex justify-content-evenly align-content-center">
                     <Button variant="danger" onClick={() => onDelete(task.id)}>Delete Task</Button>
